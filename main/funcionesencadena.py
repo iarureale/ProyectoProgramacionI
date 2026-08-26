8d2008cecbb45dbc39da76fd59ec71a36ed87b9a
Solchu_ <solanacosenza88@gmail.com>
2026-08-26 01:12:59 -0300

    Agrego ejercicio 6 del Orientador 3

---
+++ main/funcionesencadena.py
@@ -0,0 +1,10 @@
+texto = "Programacion en Python"
+
+print("a) upper:", texto.upper())
+print("b) lower:", texto.lower())
+print("c) title:", texto.title())
+print("d) capitalize:", texto.capitalize())
+print("e) replace:", texto.replace("Python", "IA"))
+print("f) count:", texto.count("a"))
+print("g) find:", texto.find("Python"))
+print("h) len:", len(texto))
