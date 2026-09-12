d9d8da60ac76ff891095b461c32b69944da50a80
juan <juancruz.isola2209@gmail.com>
2026-09-12 01:36:33 -0300

    fix: volvimos  a una versión anterior

--- main/menu.py
+++ main/menu.py
@@ -96,18 +96,18 @@ def menu_buscar_artista(lineup, nombre_artistas, codigos, tot, venta_tot, vendid
         opcion = pedir_opcion_valida("Ingresar: ", [1, 2])
 
         if opcion == 1:
-            buscado = (input("\nIngresar código o nombre del artista a buscar, (2) para salir: ")).strip()
+            buscado = (input("\nIngresar código o nombre del artista a buscar, (2) para salir: ")).strip().lower()
 
             if buscado == "2":
                 menu_buscar_artista(lineup, nombre_artistas, codigos, tot, venta_tot, vendidas_gen, vendidas_vip, escenarios, horarios)
 
             else:
-                while buscado not in nombre_artistas and buscado not in codigos and buscado != "2":
+                while buscado not in [n.lower() for n in nombre_artistas] and buscado not in [c.lower() for c in codigos] and buscado != "2":
                     print("ERROR. ingresar un código o nombre válido.")
                     opcion_a = pedir_opcion_valida("(1) Para intentarlo nuevamente, (2) para salir: ", [1, 2])
 
                     if opcion_a == 1:
-                        buscado = (input("\nIngresar código o nombre del artista a buscar: ")).strip()
+                        buscado = (input("\nIngresar código o nombre del artista a buscar: ")).strip().lower()
                     else:
                         buscado = "2"
 
@@ -219,7 +219,7 @@ def modificar_programacion(lineup, nombre_artistas, codigos, tot, venta_tot, ven
         elif modificar_datos == 3:
             horario_viejo, escenario_viejo = funciones.buscar_posicion_en_lineup(lineup, modificar_artista)
 
-            horario_nuevo = int(input("Ingresar el horario nuevo: "))
+            horario_nuevo = pedir_opcion_valida("Ingresar el horario nuevo: ", [1, 2, 3, 4, 5, 6, 7, 8])
             horario_nuevo = funciones.validar_horario(horario_nuevo, lineup, escenario_viejo)
 
             lineup[horario_viejo-1][escenario_viejo-1] = '.'
@@ -232,7 +232,7 @@ def modificar_programacion(lineup, nombre_artistas, codigos, tot, venta_tot, ven
         elif modificar_datos == 4:
             horario_viejo, escenario_viejo = funciones.buscar_posicion_en_lineup(lineup, modificar_artista)
         
-            escenario_nuevo = int(input("Ingresar el escenario nuevo: "))
+            escenario_nuevo = pedir_opcion_valida("Ingresar el horario nuevo: ", [1, 2, 3, 4, 5])
             escenario_nuevo = funciones.validar_escenario(escenario_nuevo, lineup, horario_viejo)
         
             lineup[horario_viejo-1][escenario_viejo-1] = '.'
